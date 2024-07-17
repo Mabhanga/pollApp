0f5dcedbf6086c3e37ff4e6feba79ead823ed9f7
Mabhanga <jaymotsweni@gmail,com>
2024-07-17 20:22:44 +0200

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+# Create your models here.
+class Post(models.Model):
+# Default behaviour - Django creates primary keys for you
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=140)
+    body = models.TextField()
+    signature = models.CharField(max_length=140, default='default_signature_value')
+    date = models.DateTimeField()
+
+def __str__(self):
+    return self.title
